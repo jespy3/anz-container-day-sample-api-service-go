79ae8b8085c014095211f25cebb0383b1730e17f
Josh Espinas <joshua.espinas@gmail.com>
2020-06-16 10:49:26 +1200

    change message

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World. Here are commit changes."}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
